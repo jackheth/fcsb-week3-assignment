5c05aa5a11cc57ac0002d44dfcc0b5248f08b2b8
jackheth <jackbheth@gmail.com>
2024-02-15 03:47:09 +0000

    Question15 merge branch1 to main

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/ian-knight-uofa/git-practice-01.git
 git fetch origin
 git checkout main
+git checkout branch1
+git checkout main
 git merge branch1
 git status
 git add . 
