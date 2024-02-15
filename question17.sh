d563a4cce5ad17dd8be49a6b31ca17e5f1120779
jackheth <159868430+jackheth@users.noreply.github.com>
2024-02-15 15:34:32 +1030

    Update question17.sh
    branch 1 and 2 naming fix

--- question17.sh
+++ question17.sh
@@ -2,22 +2,22 @@ git checkout -b branch1
 
 tree
 
+git checkout -b branch1
 git checkout -b branch2
-git checkout -b branch3
 
 mv git-practice-03/dir1/dir2/foo git-practice-03/dir1/dir2/foo_modified
 mv git-practice-03/dir1/dir3/bar git-practice-03/dir1/dir3/newfile2
 
 git add .
-git commit -m "branch3 updates"
+git commit -m "branch2 updates"
 
-git checkout branch2
+git checkout branch1
 mv git-practice-03/dir1/dir2/foo git-practice-03/dir1/foo
 rm git-practice-03/dir1/dir2
 touch git-practice-03/newfile1
 
 git add .
-git commit -m "branch2 updates"
+git commit -m "branch1 updates"
 
 git checkout main
 cp git-practice-03/dir1/dir3/bar git-practice-03/dir1/dir3/bar_copy
