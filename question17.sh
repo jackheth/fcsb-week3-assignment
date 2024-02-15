04de8f52cbe268b4587998079775108b7c5ec585
jackheth <159868430+jackheth@users.noreply.github.com>
2024-02-15 16:02:43 +1030

    Update question17.sh
    Major update

--- question17.sh
+++ question17.sh
@@ -1,26 +1,33 @@
+git clone https://github.com/ian-knight-uofa/git-practice-03.git
+cd git-practice-03
+
+# Create branch1
 git checkout -b branch1
 
-tree
+# Move and modify files for branch1
+mv dir1/dir2/foo dir1/foo
+rm -rf dir1/dir2
+touch newfile1
 
-git checkout -b branch1
+# Commit changes for branch1
+git add .
+git commit -m "Update directory structure for branch1"
+
+# Switch to branch2 and create it
+git checkout main
 git checkout -b branch2
 
-mv git-practice-03/dir1/dir2/foo git-practice-03/dir1/dir2/foo_modified
-mv git-practice-03/dir1/dir3/bar git-practice-03/dir1/dir3/newfile2
+# Move and modify files for branch2
+mv dir1/dir2/foo dir1/dir2/foo_modified
+mv dir3/ dir1/dir3/
+mv dir1/dir3/bar dir1/dir3/newfile2
 
+# Commit changes for branch2
 git add .
-git commit -m "branch2 updates"
+git commit -m "Update directory structure for branch2"
 
-git checkout branch1
-mv git-practice-03/dir1/dir2/foo git-practice-03/dir1/foo
-rm git-practice-03/dir1/dir2
-touch git-practice-03/newfile1
-
-git add .
-git commit -m "branch1 updates"
-
+# Switch back to main and commit changes
 git checkout main
-cp git-practice-03/dir1/dir3/bar git-practice-03/dir1/dir3/bar_copy
-
+cp dir3/bar dir3/bar_copy
 git add .
-git commit -m "main updates"
+git commit -m "Update directory structure for main"
